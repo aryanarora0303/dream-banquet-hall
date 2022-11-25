d461e1adb459b7d29f5620a477b498f45ef64f52
Aryan Arora <aryanarora@Aryans-MacBook-Pro.local>
2022-11-24 18:25:17 -0800

    Menu Package Selector added

--- js/dream.js
+++ js/dream.js
@@ -56,3 +56,183 @@ $(document).ready(function(){
       }, 1000);
     }
 });
+
+
+// Menu Package Selector Checkbox Limit
+var package = {
+    veg_appetizer_limit: 0,
+    non_veg_appetizer_limit: 0,
+    veg_dinner_limit: 0,
+    non_veg_dinner_limit: 0,
+    salads_limit: 0,
+    raita_limit: 0,
+    dessert_limit: 0
+}
+
+function updateModalLabels(package_type){
+    document.getElementById("package-modal-label").innerText = `Package ${package_type}`;
+
+    document.getElementById("veg-appetizer-checkbox-label").innerText = `Choose any ${package.veg_appetizer_limit}`;
+    document.getElementById("non-veg-appetizer-checkbox-label").innerText = `Choose any ${package.non_veg_appetizer_limit}`;
+    document.getElementById("veg-dinner-checkbox-label").innerText = `Choose any ${package.veg_dinner_limit}`;
+    document.getElementById("non-veg-dinner-checkbox-label").innerText = `Choose any ${package.non_veg_dinner_limit}`;
+    document.getElementById("salad-checkbox-label").innerText = `Choose any ${package.salads_limit}`;
+    document.getElementById("raita-checkbox-label").innerText = `Choose any ${package.raita_limit}`;
+    document.getElementById("dessert-checkbox-label").innerText = `Choose any ${package.dessert_limit}`;
+}
+
+document.getElementById("package-a-modal-controller").addEventListener("click", function() {
+    package.veg_appetizer_limit = 3;
+    package.non_veg_appetizer_limit = 3;
+
+    package.veg_dinner_limit = 3;
+    package.non_veg_dinner_limit = 3;
+
+    package.salads_limit = 3;
+    package.raita_limit = 1;
+
+    package.dessert_limit = 3;
+
+    updateModalLabels('A');
+});
+
+document.getElementById("package-b-modal-controller").addEventListener("click", function() {
+    package.veg_appetizer_limit = 3;
+    package.non_veg_appetizer_limit = 2;
+
+    package.veg_dinner = 3;
+    package.non_veg_dinner = 2;
+
+    package.salads_limit = 2;
+    package.raita_limit = 1;
+
+    package.dessert_limit = 2;
+
+    updateModalLabels('B');
+});
+
+document.getElementById("package-c-modal-controller").addEventListener("click", function() {
+    package.veg_appetizer_limit = 2;
+    package.non_veg_appetizer_limit = 2;
+
+    package.veg_dinner = 2;
+    package.non_veg_dinner = 2;
+
+    package.salads_limit = 1;
+    package.raita_limit = 1;
+
+    package.dessert_limit = 3;
+
+    updateModalLabels('C');
+});
+
+document.getElementById("package-d-modal-controller").addEventListener("click", function() {
+    package.veg_appetizer_limit = 2;
+    package.non_veg_appetizer_limit = 1;
+
+    package.veg_dinner = 2;
+    package.non_veg_dinner = 1;
+
+    package.salads_limit = 1;
+    package.raita_limit = 1;
+
+    package.dessert_limit = 1;
+
+    updateModalLabels('D');
+});
+
+$('input.veg-appetizer-checkbox').on('change', function(evt) {
+    var checked_length = document.querySelectorAll("input.veg-appetizer-checkbox:checked").length;
+    if(checked_length > package.veg_appetizer_limit) {
+        this.checked = false;
+    }
+});
+
+$('input.non-veg-appetizer-checkbox').on('change', function(evt) {
+    var checked_length = document.querySelectorAll("input.non-veg-appetizer-checkbox:checked").length;
+    if(checked_length > package.non_veg_appetizer_limit) {
+        this.checked = false;
+    }
+ });
+
+ $('input.veg-dinner-checkbox').on('change', function(evt) {
+    var checked_length = document.querySelectorAll("input.veg-dinner-checkbox:checked").length;
+    if(checked_length > package.veg_dinner_limit) {
+        this.checked = false;
+    }
+ });
+
+ $('input.non-veg-dinner-checkbox').on('change', function(evt) {
+    var checked_length = document.querySelectorAll("input.non-veg-dinner-checkbox:checked").length;
+    if(checked_length > package.non_veg_dinner_limit) {
+        this.checked = false;
+    }
+ });
+
+ $('input.salad-checkbox').on('change', function(evt) {
+    var checked_length = document.querySelectorAll("input.salad-checkbox:checked").length;
+    if(checked_length > package.salads_limit) {
+        this.checked = false;
+    }
+ });
+
+ $('input.raita-checkbox').on('change', function(evt) {
+    var checked_length = document.querySelectorAll("input.raita-checkbox:checked").length;
+    if(checked_length > package.raita_limit) {
+        this.checked = false;
+    }
+ });
+
+ $('input.dessert-checkbox').on('change', function(evt) {
+    var checked_length = document.querySelectorAll("input.dessert-checkbox:checked").length;
+    if(checked_length > package.dessert_limit) {
+        this.checked = false;
+    }
+ });
+
+ function hide_modal_to_print(){
+    document.getElementById("modal_to_print").innerHTML = "";
+    document.getElementById("modal_to_print").style.display = "none";
+ }
+
+ // Uncheck all Checkboxes when Modal Closing
+ $('#package-modal').on('hide.bs.modal', function (e) {
+    document.querySelectorAll("input:checked").forEach((el) => {
+        el.checked = false;
+    });
+    hide_modal_to_print();
+ })
+
+ function print_menu(){
+    document.getElementById("modal_to_print").innerHTML = "";
+    document.getElementById("modal_to_print").innerHTML += `<h1 class="hs-line-14 font-alt mb-50 mb-xs-30 dream-primary-color">Dream Banquet Hall</h1>`;
+    document.getElementById("modal_to_print").innerHTML += `<h1 class="hs-line-3 font-alt mb-50 mb-xs-30 dream-primary-color">Menu</h1>`;
+    document.getElementById("modal_to_print").innerHTML += `<hr/>`;
+
+    var prev_menu_item_name = "";
+    document.querySelectorAll("input:checked").forEach((el) => {
+        if(prev_menu_item_name != el.name){
+            prev_menu_item_name = el.name;
+            document.getElementById("modal_to_print").innerHTML += `<h3>${el.name}</h3>`;
+            document.getElementById("modal_to_print").innerHTML += `<p>${el.value}</p>`;
+        }
+        else {
+            document.getElementById("modal_to_print").innerHTML += `<p>${el.value}</p>`;
+        }
+
+    })
+    document.getElementById("modal_to_print").style.display = "block";
+
+    $("#modal_to_print").printThis({ 
+        debug: false,              
+        importCSS: true,             
+        importStyle: true,         
+        printContainer: true,       
+        loadCSS: ["./css/bootstrap.min.css", "./css/style.css", "./css/dream.css"], 
+        pageTitle: "Dream Banquet Hall Menu",             
+        removeInline: false,        
+        printDelay: 30,            
+        header: null,             
+        formValues: true          
+    });
+ }
